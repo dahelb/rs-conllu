82c1aa2a7287bb049cdfc48796293a002140a480
David Helbig <52451401+davidhelbig@users.noreply.github.com>
2024-08-27 19:21:12 +0200

    Add SentenceBuilder struct

--- src/token.rs
+++ src/token.rs
@@ -5,7 +5,7 @@ use crate::UPOS;
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub enum TokenID {
     /// The standard, single index.
